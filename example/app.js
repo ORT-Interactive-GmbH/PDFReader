2bf2c413582435462aeb33c782ce95153f46a37d
Hans Knöchel <hans@Air-von-Hans.fritz.box>
2021-08-25 21:58:36 +0200

    chore: update meta files

--- example/app.js
+++ example/app.js
@@ -1,14 +1,11 @@
+import PDFReader from 'de.ortinteractive.pdfreader';
 
-var win = Titanium.UI.createWindow();
+const window = Ti.UI.createWindow();
 
-var pdfReader = require("de.ortinteractive.pdfreader" );
-var reader = pdfReader.createReader({
-	top: 0,
-	left: 0,
-	width: Ti.UI.FILL,
-	height: Ti.UI.FILL,
-	pdf: Ti.Filesystem.getFile( Ti.Filesystem.resourcesDirectory , "sample.pdf" ).resolve(),
-	password: ""
+const reader = PDFReader.createReader({
+	pdf: Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory , 'sample.pdf'),
+	password: ''
 });
-win.add( reader );
-win.open();
+
+window.add(reader);
+window.open();
